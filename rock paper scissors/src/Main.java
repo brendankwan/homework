bca663be2327568560d75c1bceb46954382dd2b2
Brendan Kwan <2125331@learn.vsb.bc.ca>
2021-02-11 14:41:45 -0800

    rock paper scissors

---
+++ rock paper scissors/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]args){
+        // game logic
+        //rock beats scissors
+        //paper beats rock
+        //scissors beats paper
+        //System.out.println((int)(Math.random()*3)*1);
+        String rock = 1;
+
+    }
+}
