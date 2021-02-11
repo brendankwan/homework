46db44427b1e3109da614fb0aa33b299bce59527
brendankwan <78882081+brendankwan@users.noreply.github.com>
2021-02-10 18:20:34 -0800

    homework

---
+++ schedule/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        String Table ="+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +";
+        String firstC="| 1 |                       Chemistry 11|                       Ms.ogden|";
+        String seconC="| 2 |                    Pre-calculus 11|                         Mr.Tam|";
+        String thirdC="| 3 |                Senior Concert Band|                   Mr.Robertson|";
+        String fourtC="| 4 |                         Physics 11|                        Mr.Jung|";
+        String FifthC="| 5 |                     Composition 11|                  Mr.Tryssenaar|";
+        String sixthC="| 6 |            Computer Programming 11|                     Mr.Zaremba|";
+        String SevenC="| 7 |                        Mandarin 11|                       Ms.Chang|";
+        String EightC="| 8 |                       Geography 12|                    Ms.Clutchey|";
+
+        System.out.println(Table);
+        System.out.println(firstC);
+        System.out.println(seconC);
+        System.out.println(thirdC);
+        System.out.println(fourtC);
+        System.out.println(FifthC);
+        System.out.println(sixthC);
+        System.out.println(SevenC);
+        System.out.println(EightC);
+        System.out.println(Table);
+
+    }
+}
---
+++ variables and names/src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main( String[] args ){
+        int first;
+        first = 113;
+        double second ;
+        second = 2.71828;
+        String name;
+        name = "Computer Science";
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(name);
+
+
+    }
+}
