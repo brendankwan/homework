a1e1f57e0e115728b0cdad6c8b4ada562a0be767
Brendan Kwan <2125331@learn.vsb.bc.ca>
2021-02-11 13:35:46 -0800

    true or false

--- Boolean/src/Main.java
+++ Boolean/src/Main.java
@@ -14,11 +14,16 @@ public class Main {
         y = 20;
         int z = 30;
         System.out.println((( x == y) || (x < z)) && (x*2 + y)/5 > z);
-
+        System.out.println(((10 == y) || (10 <z ))&&  (10*2+y)/5 > z);
+        System.out.println(((10 == 20)|| (10 <z)) && (10*2+20)/5 > z);
+        System.out.println(((10 == 20)|| (10 <30))&& (10*2+20)/5>30);
+        System.out.println(((10 == 20)|| (10 <30))&& (20+20)/5>30);
         //Question 3
         boolean a = true;
         boolean b = false;
         System.out.println((a || b) && ((a == b) || a));
+        System.out.println((true||b)&&((a == b)|| a));
+        System.out.println((true||false)&&((true==false)|| true));
 
         //Question 4
         a = false;
@@ -26,8 +31,18 @@ public class Main {
         x = 5;
         y = 10;
         System.out.println((a == b) && (x * 4) == (y % 6) * 5);
+        System.out.println((a == b) && (5*4) == (y % 6) *5);
+        System.out.println((a == b) && (5*4) == (10 % 6) *5);
+        System.out.println((false == b) && (5*4) == (10 % 6)* 5);
+        System.out.println((false == false)&& (5*4) == (10 % 6)* 5);
+        System.out.println((false == false)&& (20) == (10 % 6) * 5);
+        System.out.println((false == false)&& (20) == (4)*5);
 
         //Question 5
         System.out.println((Math.abs(-2) == 2) || (Math.sqrt(Math.pow(9,2)) <= 9 && (-4 <= 4)));
+        System.out.println(((2) == 2) || (Math.sqrt(Math.pow(9,2)) <= 9 && (-4 <= 4)));
+        System.out.println(((2) == 2) || (Math.sqrt(81) <= 9 && (-4 <= 4)));
+        System.out.println(((2) == 2) || (9)<= 9 && (-4 <= 4));
+     
     }
 }
