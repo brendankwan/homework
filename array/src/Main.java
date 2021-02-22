cfd1cc2d5c75b49a7a64e7b92b463a0f25b62a23
Brendan Kwan <2125331@learn.vsb.bc.ca>
2021-02-22 14:59:12 -0800

    Array assignment

--- array/src/Main.java
+++ array/src/Main.java
@@ -1,10 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = new int [10];
+        int[] numbers = new int[20];
 
-        for (int i = 0; i<numbers.length ; i++){
-            numbers[i] = i*2;
-            System.out.println("indoex:" + i + " value:" + numbers[i]);
+        for (int i = 0; i < 20;i++) {
+            numbers[i] = (int) ((Math.random() * 6) + 1);
+            System.out.println("index:" + i + " value:" + numbers[i]);
+        }
+            int[] numbers2 = new int[10];
+            for (int i = 0; i < 10; i++) {
+                numbers2[i] = (int) ((Math.random() * 100) + 1);
+                System.out.println("index:" + i + " value:" + numbers2[i]);
+
+
+
         }
 
     }
