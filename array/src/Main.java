6432ae28a91d9d38da3f1d262bc7acaa19e54b98
Brendan Kwan <2125331@learn.vsb.bc.ca>
2021-02-23 14:32:33 -0800

    array

--- array/src/Main.java
+++ array/src/Main.java
@@ -1,19 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         int[] numbers = new int[20];
-
-        for (int i = 0; i < 20;i++) {
+        int count = 0;
+        for (int i = 0; i < 20; i++) {
             numbers[i] = (int) ((Math.random() * 6) + 1);
-            System.out.println("index:" + i + " value:" + numbers[i]);
+            if (numbers[i]==1) {
+                count++;
+            } System.out.println("index:" + i + " value:" + numbers[i]);
+            System.out.println("count:"+count);
         }
-            int[] numbers2 = new int[10];
-            for (int i = 0; i < 10; i++) {
-                numbers2[i] = (int) ((Math.random() * 100) + 1);
-                System.out.println("index:" + i + " value:" + numbers2[i]);
 
+        int[] numbers2 = new int[10];
 
-
+        for (int i = 0; i < numbers2.length; i++) {
+            numbers2[i] = (int) ((Math.random() * 100) + 1);
+            int average = numbers2[i]/numbers2.length;
+            System.out.println("Average:"+average);
+            System.out.println("index:" + i + " value:" + numbers2[i]);
         }
+
 
     }
 }
