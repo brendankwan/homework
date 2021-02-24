036c4ce30458b3670a4c8a1bd2f669f833163e3b
Brendan Kwan <2125331@learn.vsb.bc.ca>
2021-02-24 15:00:47 -0800

    array method assignment

--- Array method assignment/src/Main.java
+++ Array method assignment/src/Main.java
@@ -9,19 +9,38 @@ public class Main {
         //addon the number to last position
         return newarray;
     }
+
+
+
+
+
+
     static double pyth(double a, double b){
         return squareRoot(a*a + b*b);
     }
     static double squareRoot(double num){
         //check algorithm
+        //square root of num is equal ans when ans * ans - num
+        double epsilon = 0.0001;
+        double ans = 0;
+        double increment = 0.001;
+        int count = 0;
+        while (Math.sqrt(ans*ans-num)< epsilon) {
+            ans+= increment;
+            count ++;
+        }
+        if(Math.sqrt(ans*ans - num) < epsilon) {
+            System.out.println("Found square at count:" + count + "It is: " + ans);
+            return ans;
+        }
+        else
+            System.out.println("Failed to find square root.");
+            return ans;
+
     }
 
     public static void main( String[] args ){
-      int[] numbers = new int[2];
-      numbers[0] = 5;
-      numbers[1] = 6;
-      numbers = addOn(numbers, 2);
-
+      System.out.println(squareRoot(25));
     }
 
 }
