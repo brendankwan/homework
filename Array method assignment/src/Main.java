8ba82a99a989328588c48a49f95b5e7e204038cd
Brendan Kwan <2125331@learn.vsb.bc.ca>
2021-02-24 14:16:35 -0800

    array method assignment

---
+++ Array method assignment/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+//parameter for now has to be an positive integer perfect squares
+    //if the method doe not find a square root return -1
+    //find sqroot of any number
+    public static int[] addOn(int[]array, int number){
+        int[]newarray = new int [array.length+1];
+        //loop thru array
+        //copy old array to new array
+        //addon the number to last position
+        return newarray;
+    }
+    static double pyth(double a, double b){
+        return squareRoot(a*a + b*b);
+    }
+    static double squareRoot(double num){
+        //check algorithm
+    }
+
+    public static void main( String[] args ){
+      int[] numbers = new int[2];
+      numbers[0] = 5;
+      numbers[1] = 6;
+      numbers = addOn(numbers, 2);
+
+    }
+
+}
