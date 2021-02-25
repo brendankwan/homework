53108ecede7792d9d5d3fbd167ed95dfef7e07b7
Brendan Kwan <2125331@learn.vsb.bc.ca>
2021-02-25 13:54:27 -0800

    array method

--- Array method assignment/src/Main.java
+++ Array method assignment/src/Main.java
@@ -1,13 +1,25 @@
 public class Main {
 //parameter for now has to be an positive integer perfect squares
-    //if the method doe not find a square root return -1
-    //find sqroot of any number
-    public static int[] addOn(int[]array, int number){
-        int[]newarray = new int [array.length+1];
+
+
+
+    public static int[] addOn(int[]array, int value){
+        System.out.println("Add new value to the end of an array");
+        int[]newarray = new int[array.length+1];
+        for(int i = 0; i < array.length; i++){
+
+        }
+        newarray[newarray.length-1] = value;
         //loop thru array
         //copy old array to new array
         //addon the number to last position
         return newarray;
+   }
+    public static int[] delete(int[]array, int value){
+        System.out.println("Delete ");
+        int[]newarray = new int[array.length - 1];
+
+
     }
 
 
@@ -15,32 +27,9 @@ public class Main {
 
 
 
-    static double pyth(double a, double b){
-        return squareRoot(a*a + b*b);
-    }
-    static double squareRoot(double num){
-        //check algorithm
-        //square root of num is equal ans when ans * ans - num
-        double epsilon = 0.0001;
-        double ans = 0;
-        double increment = 0.001;
-        int count = 0;
-        while (Math.sqrt(ans*ans-num)< epsilon) {
-            ans+= increment;
-            count ++;
-        }
-        if(Math.sqrt(ans*ans - num) < epsilon) {
-            System.out.println("Found square at count:" + count + "It is: " + ans);
-            return ans;
-        }
-        else
-            System.out.println("Failed to find square root.");
-            return ans;
-
-    }
 
     public static void main( String[] args ){
-      System.out.println(squareRoot(25));
+
     }
 
 }
