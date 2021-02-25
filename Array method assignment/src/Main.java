2ce9076c5c4498d6c5347cdc54452b38d160793e
Brendan Kwan <2125331@learn.vsb.bc.ca>
2021-02-25 14:09:11 -0800

    array method

--- Array method assignment/src/Main.java
+++ Array method assignment/src/Main.java
@@ -2,34 +2,24 @@ public class Main {
 //parameter for now has to be an positive integer perfect squares
 
 
-
-    public static int[] addOn(int[]array, int value){
+    public static int[] addOn(int[] array, int value) {
         System.out.println("Add new value to the end of an array");
-        int[]newarray = new int[array.length+1];
+        int[] newarray = new int[array.length + 1];
         for(int i = 0; i < array.length; i++){
-
+            newarray[i] = array[i];
         }
-        newarray[newarray.length-1] = value;
+        newarray[newarray.length - 1] = value;
         //loop thru array
         //copy old array to new array
         //addon the number to last position
         return newarray;
-   }
-    public static int[] delete(int[]array, int value){
-        System.out.println("Delete ");
-        int[]newarray = new int[array.length - 1];
+    }
+
+    
+
+
+    public static void main(String[] args) {
 
 
     }
-
-
-
-
-
-
-
-    public static void main( String[] args ){
-
-    }
-
 }
