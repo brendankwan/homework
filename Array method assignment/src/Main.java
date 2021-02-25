56c0a8665dfacc25553aba615a206cf4e8f9727f
Brendan Kwan <2125331@learn.vsb.bc.ca>
2021-02-25 14:45:19 -0800

    array method

--- Array method assignment/src/Main.java
+++ Array method assignment/src/Main.java
@@ -20,6 +20,11 @@ public class Main {
 
 
     public static void main(String[] args) {
+    int[]array = new int [5];
+    for(int i = 0; i< array.length; i++){
+     array[i] = (int)(Math.random()*4);
+    }
+
 
 
 
