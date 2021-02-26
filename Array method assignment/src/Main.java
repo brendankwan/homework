3a5ab473ccbad4f8118052beaa5a17cd3f787fd3
brendankwan <78882081+brendankwan@users.noreply.github.com>
2021-02-25 21:53:34 -0800

    array method

--- Array method assignment/src/Main.java
+++ Array method assignment/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 
     public static int[] addOn(int[] array, int value) {
-        System.out.println("Add new value to the end of an array");
+
         int[] newarray = new int[array.length + 1];
         for(int i = 0; i < array.length; i++){
             newarray[i] = array[i];
@@ -15,17 +15,58 @@ public class Main {
         return newarray;
 
     }
+    public static int[] delete(int[]array){
+       int [] newarray2 = new int [array.length-1];
+        for (int i = 0; i< newarray2.length; i++){
+            newarray2[i] = array[i];
+        }
+        return newarray2;
+    }
+    public static int[] insert(int[]array,int value){
+        int [] newarray3 = new int [array.length +1];
 
+        for (int i = 0; i < array.length; i++){
+            newarray3[i] = array[i];
 
+        }
+        newarray3[] = value;
+        return newarray3;
+    }
 
 
 
     public static void main(String[] args) {
     int[]array = new int [5];
     for(int i = 0; i< array.length; i++){
-     array[i] = (int)(Math.random()*4);
+     array[i] = i+1;
         System.out.println("index:" + i + "value:" + array[i]);
     }
+    int[]addNumberTo = addOn(array, 1);
+    for(int number : addNumberTo){
+        System.out.println(number);
+    }
+    int [] array2 = new int[5];
+    for (int i = 0;i < array.length;i++){
+        array2[i] = i+1;
+        System.out.println("index:" + i + "value:" + array2[i]);
+    }
+    int[] delete = delete(array2);
+        System.out.println();
+        for (int i = 0;i < delete.length;i++){
+            array2[i] = i+1;
+            System.out.println("index:" + i + "value:" + delete[i]);
+        }
+        int[] array3 = new int[5];
+        for (int i = 0; i < array3.length; i ++){
+            array3[i] = i + 1 ;
+            System.out.println("index:" + i + "value:" + array3[i]);
+        }
+        int[]insert = insert(array3,3);
+        for(int i = 0; i< insert.length; i++){
+            array3[i] = i + 1;
+            System.out.println("index:" + i + "value:" + insert[i]);
+        }
+
 
 
 
