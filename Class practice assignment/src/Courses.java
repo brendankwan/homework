24b452b2cd7dccab1da4e3e1e07dbf7504fc3bbe
Brendan Kwan <2125331@learn.vsb.bc.ca>
2021-03-08 14:52:32 -0800

    Class practice assignment

--- Class practice assignment/src/Courses.java
+++ Class practice assignment/src/Courses.java
@@ -1,3 +1,22 @@
 public class Courses {
-    
+
+    private String subjectName;
+    private String studentGrade;
+
+    public Courses(String subjectName,String studentGrade){
+        this.subjectName = subjectName;
+        this.studentGrade = studentGrade;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public String getStudentGrade() {
+        return studentGrade;
+    }
+    public String toString(){
+        return "Subject name: " + subjectName;
+    }
+
 }
--- Class practice assignment/src/Student.java
+++ Class practice assignment/src/Student.java
@@ -1,3 +1,15 @@
 public class Student {
+    private String firstName;
+    private static int studentId;
+    private String[] courses = new String[8];
+
+    public Student(String firstName, int studentId, String[]courses){
+        this.firstName = firstName;
+        this.studentId = studentId++;
+        this.courses = courses;
+    }
+
+
+
 
 }
