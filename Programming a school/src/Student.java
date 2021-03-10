1070ab363b16b8d4b030c41f0d4bf391216e807b
Brendan Kwan <2125331@learn.vsb.bc.ca>
2021-03-10 13:41:31 -0800

    Programming a school

--- Programming a school/src/Student.java
+++ Programming a school/src/Student.java
@@ -1,5 +1,18 @@
 public class Student {
 
+    private String firstName;
+    private String lastName;
+    private int grade;
+    private static int studentNum = (int)(Math.random()*12345678);
+    private int num;
+
+    public Student(String firstName, String lastName, int grade){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grade = grade;
+        this.studentNum += (int)(Math.random()*12345678);
+        
+    }
 
 
 
