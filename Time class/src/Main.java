5c509c9e89e7cb15386871d7133ad8917cb1306e
Brendan Kwan <2125331@learn.vsb.bc.ca>
2021-03-12 14:28:35 -0800

    time class

--- Time class/src/Main.java
+++ Time class/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
 
+    public static void main(String[] args) {
+            Time time = new Time(27,61,61);
+        System.out.println(time);
+        time.setHour(27);
+        time.setMinute(61);
+        time.setSecond(61);
+        System.out.println(time);
+
+
+    }
 
 
 
--- Time class/src/Time.java
+++ Time class/src/Time.java
@@ -1,4 +1,7 @@
 public class Time {
+    private String zero1;
+    private String zero2;
+    private String zero3;
     private int hour;
     private int minute;
     private int second;
@@ -7,22 +10,12 @@ public class Time {
 
 
     public Time(int hour, int minute, int second){
-        if(second >= 60){
-            count = (int)(second/60);
-            minute = minute + count;
-            second = second - 60*count;
-        }
-        if(minute >= 60 ){
-            count = (int)(minute/60);
-            minute = minute - 60*count;
-            hour = hour + count;
-        }
-        if(hour >= 24){
-            count = (int)(hour/24);
-            hour = 
-
-        }
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
     }
+
+
 
     public int getHour() {
         return hour;
@@ -35,31 +28,76 @@ public class Time {
     public int getSecond() {
         return second;
     }
+    public String addZeroToHour(){
+        if(hour<9){
+            zero1 = "0";
+        }
+        
+    }
 
     public void setHour(int hour) {
 
+        if(hour >= 24){
+            count = (int)(hour/24);
+            hour = hour - 24*count;
+        }
         this.hour = hour;
     }
 
     public void setMinute(int minute) {
+        if(minute >= 60 ){
+            count = (int)(minute/60);
+            minute = minute - 60*count;
+            hour = hour + count;
+        }
         this.minute = minute;
     }
 
     public void setSecond(int second) {
+        if(second >= 60){
+            count = (int)(second/60);
+            minute = minute + count;
+            second = second - 60*count;
+        }
         this.second = second;
+
     }
 
+
+
+
+
+
     public void setTime(int hour, int minute, int second){
+        if(second >= 60){
+            count = (int)(second/60);
+            minute = minute + count;
+            second = second - 60*count;
+        }
+        this.second = second;
+        if(minute >= 60 ){
+            count = (int)(minute/60);
+            minute = minute - 60*count;
+            hour = hour + count;
+        }
+        this.minute = minute;
+        if(hour >= 24){
+            count = (int)(hour/24);
+            hour = hour - 24*count;
+        }
+        this.hour = hour;
 
     }
     public String toString(){
+        return "" + hour + minute + second;
+
 
     }
 
-    public Time nextSecond(){
+    //public Time nextSecond(){
 
-    }
-    public Time previousSecond(){
+    //}
+    //public Time previousSecond(){
 
-    }
+    //}
 }
