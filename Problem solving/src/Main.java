a95dc36e3ae6ceec7760d4b3b8a4f92c1fdceee2
brendankwan <78882081+brendankwan@users.noreply.github.com>
2021-04-05 17:34:57 -0700

    first problem solved

--- Problem solving/src/Main.java
+++ Problem solving/src/Main.java
@@ -2,13 +2,13 @@ public class Main {
     //Code your solution to problem number one here
     static int problemOne(String s){
         int answer = 0;
-
+        //your code here
         int length = s.length();
         String aeiou = "aeiou";
-        for (int i = 0; i < length; i ++){
+        for(int i = 0 ; i < length; i ++)  {
             String letter = s.substring(i,i+1);
             if(aeiou.contains(letter)){
-                answer = answer+1;
+                answer = answer + 1;
             }
         }
         return answer;
@@ -16,12 +16,13 @@ public class Main {
     //Code you problem number two here
     static int problemTwo(String s){
         int answer = 0;
-
+        //your code here
+        
         return answer;
     }
     //Code your solution to problem number 3 here
     static String problemThree(String s){
-
+        //your code here
         return s;
     }
     public static void main(String[] args) {
