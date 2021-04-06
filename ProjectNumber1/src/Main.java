4a2dc8409c112a99f708ef60701f09bd69a10c3a
brendankwan <78882081+brendankwan@users.noreply.github.com>
2021-04-05 17:23:41 -0700

    Project Number 1

---
+++ ProjectNumber1/src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    //Code your solution to problem number one here
+    static int problemOne(String s){
+        int answer = 0;
+
+        int length = s.length();
+        String aeiou = "aeiou";
+        for (int i = 0; i < length; i ++){
+            String letter = s.substring(i,i+1);
+            if(aeiou.contains(letter)){
+                answer = answer+1;
+            }
+        }
+        return answer;
+    }
+    //Code you problem number two here
+    static int problemTwo(String s){
+        int answer = 0;
+
+        return answer;
+    }
+    //Code your solution to problem number 3 here
+    static String problemThree(String s){
+
+        return s;
+    }
+    public static void main(String[] args) {
+        /*
+        Set s to a string and run your method using s as the parameter
+        Run your method in a println statement to determine what the output was
+        Once you think you have it working try running the tests.
+        The tests will put your method through several different Strings to test
+        all possible cases.  If you have 100% success then there is no bugs in your methods.
+         */
+        String s;
+    }
+}
