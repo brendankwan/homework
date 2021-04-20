ef87c862cdc6d97997ef199c8e33db59020451bf
Brendan Kwan <2125331@learn.vsb.bc.ca>
2021-04-20 13:27:23 -0700

    read text

---
+++ read text/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+
+
+
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> lineS = new ArrayList<>();
+        FileReader fr = new FileReader("ProgrammingHistory.txt");
+        BufferedReader br = new BufferedReader(fr);
+        
+
+
+
+    }
+}
