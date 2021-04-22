9032803d2fb859d73ef906d3a04939a373828a15
Brendan Kwan <2125331@learn.vsb.bc.ca>
2021-04-22 14:58:33 -0700

    ye bro

--- read text/src/Main.java
+++ read text/src/Main.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class Main {
 
-
+    public static
 
     public static void main(String[] args) throws IOException {
         ArrayList<String> lines = new ArrayList<>();
